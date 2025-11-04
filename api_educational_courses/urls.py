4bdfdd18fd5f6de0b0d699f6a8e4cee1e656d591
TarantinoQuentin <gregdenfv42@gmail.com>
2025-11-04 13:41:19 +0300

    Начало положено, не работает аутентификация

--- api_educational_courses/urls.py
+++ api_educational_courses/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import CourseGenericAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
